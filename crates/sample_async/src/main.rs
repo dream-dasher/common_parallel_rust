d965bb8462041cad0cdd5a5f5e096ec7cb208035
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2025-01-12 21:38:42 +0100

    chore(deps): hard update

--- crates/sample_async/src/main.rs
+++ crates/sample_async/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-        println!("Hello, world!");
+//! Exploring some tokio init code.
+//!
+//! ## Note
+//! **tokio** is not compatible with wasm target.
+
+#[cfg(target_arch = "wasm32")]
+fn main() {}
+#[cfg(not(target_arch = "wasm32"))]
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+        // Needs:
+        // - trait `IntoUrl`
+        //   - pre-impl for `String` and `&str`
+        const URL_DEFAULT: &str = "https://httpbin.org";
+
+        // Basic
+        let client = reqwest::Client::new();
+
+        let res = client
+                .post("http://httpbin.org/post")
+                .body("the exact body that is sent")
+                .send()
+                .await?;
+
+        println!("Response status {}", res.status());
+        Ok(())
 }
