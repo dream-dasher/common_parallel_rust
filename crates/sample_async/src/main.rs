6a31769e84615119df39a68b83e188e79cd10bd3
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2025-01-12 22:24:14 +0100

    wip: org, infra

--- crates/sample_async/src/main.rs
+++ crates/sample_async/src/main.rs
@@ -3,25 +3,46 @@
 //! ## Note
 //! **tokio** is not compatible with wasm target.
 
-#[cfg(target_arch = "wasm32")]
-fn main() {}
+mod support;
+
+use std::time::Duration;
+
+use support::Result;
 #[cfg(not(target_arch = "wasm32"))]
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
+async fn main() -> Result<()> {
+        use support::activate_global_default_tracing_subscriber;
+        let _writer_guard = activate_global_default_tracing_subscriber(None, None)?;
+
         // Needs:
         // - trait `IntoUrl`
         //   - pre-impl for `String` and `&str`
         const URL_DEFAULT: &str = "https://httpbin.org";
 
-        // Basic
-        let client = reqwest::Client::new();
+        // client:
+        // - `Arc` used internally
+        let client = reqwest::Client::builder()
+                .https_only(true) // this will error for `http` (WARN: not compile-time checked)
+                .use_rustls_tls()
+                // .default_headers(headers)
+                .timeout(Duration::from_secs(30))
+                .build()?;
 
-        let res = client
-                .post("http://httpbin.org/post")
+        let request = client
+                .post("https://httpbin.org/post")
                 .body("the exact body that is sent")
-                .send()
-                .await?;
+                .build()?;
 
-        println!("Response status {}", res.status());
+        println!("Method: {:?}", request.method());
+        println!("URL: {:?}", request.url());
+        println!("Headers: {:?}", request.headers());
+        println!("Body: {:?}", request.body());
+
+        let response = client.execute(request).await?;
+
+        println!("Response status {}", response.status());
         Ok(())
 }
+
+#[cfg(target_arch = "wasm32")]
+fn main() {}
