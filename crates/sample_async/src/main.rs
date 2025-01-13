b3a5f390b105129115689c829a69d4ce5ad9ddba
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2025-01-13 04:13:23 +0100

    wip: parallel requests

--- crates/sample_async/src/main.rs
+++ crates/sample_async/src/main.rs
@@ -17,6 +17,7 @@ mod support;
 use std::time::Duration;
 
 use reqwest::{Method, Url, header::HeaderMap};
+use serde::{Deserialize, Serialize};
 use support::{Result, activate_global_default_tracing_subscriber};
 use tracing::{Level as L, event as tea};
 
@@ -25,7 +26,7 @@ use tracing::{Level as L, event as tea};
 async fn main() -> Result<()> {
         let _writer_guard = activate_global_default_tracing_subscriber(None, None)?;
 
-        // Needs:
+        // # `Url`
         // - (sealed) trait `IntoUrl`
         //   - impl for `String` and `&str`
         // - almost everything is a re-export of `Url`
@@ -44,7 +45,7 @@ async fn main() -> Result<()> {
         tea!(L::DEBUG, ?base_httpbin, ?base_typicode, ?todos_typicode, ?delay_httpbin, ?json_httpbin);
         tea!(L::INFO, a_url=?json_httpbin.as_str());
 
-        // header-module
+        // # header-module
         // - HeaderName
         // - HeaderMap
         // - various constant header names available, but don't seem necessary
@@ -55,8 +56,8 @@ async fn main() -> Result<()> {
                 headers.insert("User-Agent", "rust-reqwest-client".parse().unwrap());
                 headers
         };
-        // xhs httpbin.org/headers accept:'application/json' Authorization:'prettyplease' Fanciful:'yesm'
-        // `Client`
+
+        // # `Client`
         // - prefer `::builder()`
         //   - alt: `::new()` is effectively `::default()`
         //   - see ClientBuilder
@@ -69,6 +70,7 @@ async fn main() -> Result<()> {
                 .timeout(Duration::from_secs(30)) // default is *no* timeout
                 .build()?;
 
+        // # `Request`
         // see RequestBuilder
         let request = client
                 .request(Method::GET, base_httpbin.join("/headers")?)
@@ -79,7 +81,7 @@ async fn main() -> Result<()> {
                 .build()?;
         tea!(L::DEBUG, ?request);
 
-        // Response:
+        // # Response:
         // - status
         //   - error_for_status
         // - cookies
@@ -87,10 +89,121 @@ async fn main() -> Result<()> {
         // - text
         // - json
         let response = client.execute(request).await?;
-
         tea!(L::DEBUG, ?response);
         tea!(L::INFO, resp_status=?response.status());
+
+        // # JSON, typed
+        // (see struct `Todo` below)
+        {
+                let response = client
+                        .request(Method::GET, base_typicode.join("/todos")?)
+                        .query(&[("userId", "1"), ("completed", "false")])
+                        .send()
+                        .await?;
+
+                let todos_type_matched: Vec<Todo> = response.json().await?;
+                tea!(L::INFO, "Retrieved {} todos", todos_type_matched.len());
+                tea!(L::DEBUG, two_todos = ?todos_type_matched.get(0..=1));
+                tea!(L::DEBUG, ?todos_type_matched);
+                println!("-------,\nFirst Todo, Type-Matched:\n{:#?}\n-------", todos_type_matched.first());
+        }
+        // # JSON, ad hoc
+        {
+                let response = client
+                        .request(Method::GET, base_typicode.join("/todos")?)
+                        .query(&[("userId", "1"), ("completed", "false")])
+                        .send()
+                        .await?;
+
+                let todos_ad_hoc: serde_json::Value = response.json().await?;
+                tea!(L::DEBUG, ?todos_ad_hoc);
+                println!("-------,\nFirst Todo, Ad Hoc Construction:\n{:#?}\n-------", todos_ad_hoc.get(0));
+        }
+
+        // # Parallel requests with `Futures`
+        {
+                use futures::future;
+
+                let urls = vec![
+                        base_httpbin.join("/delay/3")?,
+                        base_httpbin.join("/delay/2")?,
+                        base_httpbin.join("/delay/3")?,
+                        base_httpbin.join("/delay/2")?,
+                        base_httpbin.join("/delay/3")?,
+                        base_httpbin.join("/delay/2")?,
+                        base_httpbin.join("/delay/3")?,
+                        base_httpbin.join("/delay/2")?,
+                        base_httpbin.join("/delay/3")?,
+                        base_httpbin.join("/delay/2")?,
+                ];
+
+                // ## `futures::join_all`
+                // - single future gives a single return of a vec (e.g. of results)
+                // - no concurrency limit
+                // - held up by longest running component future
+                // - will hold onto memory for all futures whiel it waits
+                // - for large requests it will use `FuturedOrdered` (instead of Unordered)
+                {
+                        let futures: Vec<_> = urls
+                                .iter()
+                                .map(|url| client.request(Method::GET, url.clone()).send())
+                                .collect();
+
+                        let start_timer = std::time::Instant::now();
+                        let results = future::join_all(futures).await;
+                        for result in results.iter() {
+                                tea!(L::DEBUG, ?result);
+                        }
+                        let time_passed = start_timer.elapsed();
+                        tea!(L::DEBUG, "All requests completed in {:?}", time_passed);
+                        println!(
+                                "`Join_All`: {} results returned, each with a delay of 2 or 3 seconds, in a total of {} seconds.",
+                                results.len(),
+                                time_passed.as_secs_f64()
+                        );
+                }
+                // ## `futures::stream`
+                // Parallel requests with option for max concurrent requests
+                {
+                        use futures::stream::{self, StreamExt};
+
+                        const BUFFER_SIZE: usize = 2;
+                        let start_timer = std::time::Instant::now();
+                        let mut count = 0;
+                        let stream = stream::iter(urls)
+                                .map(|url| client.get(url).send())
+                                // .boxed()
+                                .buffer_unordered(BUFFER_SIZE) // Only 2 requests in flight at once
+                                // .buffered(2) // yields responses only in the order futures were arranged
+                                .collect::<Vec<_>>()
+                                .await;
+
+                        for res in stream {
+                                count += 1;
+                                tea!(L::DEBUG, ?res);
+                        }
+                        let time_passed = start_timer.elapsed();
+                        tea!(L::DEBUG, "All requests completed in {:?}", time_passed);
+                        println!(
+                                "`Stream.buffer_unordered({})`: {} results streamed back, each with a delay of 2 or 3 seconds, in a total of {} seconds.",
+                                BUFFER_SIZE,
+                                count,
+                                time_passed.as_secs_f64()
+                        );
+                }
+        }
         Ok(())
+}
+
+/// Struct to pull typicode responses into
+/// Example of using 'typed' JSON with Serde
+#[derive(Debug, Serialize, Deserialize)]
+struct Todo {
+        #[serde(rename = "userId")]
+        user_id:   i32,
+        id:        i32,
+        title:     String,
+        completed: bool,
 }
 
 // #[cfg(target_arch = "wasm32")]
