3f8db98c1cb65c9cd03f5477d662fcb89d8d09f6
Ethan Skowronski-Lutz <33399972+dream-dasher@users.noreply.github.com>
2025-03-18 15:12:51 -0400

    perf(dev): used features only
    Features declared in workspace Cargo.toml are
    non-optionally inherited.

--- crates/sample-async/src/main.rs
+++ crates/sample-async/src/main.rs
@@ -33,7 +33,7 @@ use tracing::{debug, error, info, warn};
 use utilities::activate_global_default_tracing_subscriber;
 
 // #[cfg(not(target_arch = "wasm32"))]
-#[tokio::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> SampleResult<()> {
         let _writer_guard = activate_global_default_tracing_subscriber()
                 .maybe_default_logging_level(None)
