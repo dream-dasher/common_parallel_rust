ec11e0c82c7e12523e6da83fd3d351b64fb45053
Ethan Skowronski-Lutz <33399972+dream-dasher@users.noreply.github.com>
2025-04-14 16:00:05 -0700

    fix: removed webapp entirely (wasm comp too buggy)

--- crates/sample-webapp/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Library
-
-mod app;
-mod error;
-
-pub use app::WebCompatibleApp;
-pub use error::{ErrKind, ErrWrapper, ToOther};
-
-pub type WebCompatibleResult<T> = std::result::Result<T, ErrWrapper>;
